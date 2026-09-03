3a46a9f27635c2338c27d5fb8927f56ed6920154
Sean Murphy <sean@iamseanmurphy.com>
2026-09-03 09:46:10 +0000

    1.5.6

--- advanced-faq-manager/advanced-faq-manager.php
+++ advanced-faq-manager/advanced-faq-manager.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: Advanced FAQ Manager (Best FAQ Plugin for WordPress)
  * Description: FAQ Plugin for WordPress lets you create and manage FAQs in your WordPress pages. 
- * Version:     1.5.5
+ * Version:     1.5.6
  * Author:      ThemeHigh
  * Author URI:  https://www.themehigh.com
  * Plugin URI:  https://www.themehigh.com
@@ -32,7 +32,7 @@ if(!class_exists('THFAQF_Manager')){
 		}
 
 		public function init() {
-			define('THFAQF_VERSION', '1.5.5');
+			define('THFAQF_VERSION', '1.5.6');
 			!defined('THFAQF_BASE_NAME') && define('THFAQF_BASE_NAME', plugin_basename( __FILE__ ));
 			!defined('THFAQF_PATH') && define('THFAQF_PATH', plugin_dir_path( __FILE__ ));
 			!defined('THFAQF_URL') && define('THFAQF_URL', plugins_url( '/', __FILE__ ));
--- advanced-faq-manager/readme.txt
+++ advanced-faq-manager/readme.txt
@@ -3,8 +3,8 @@ Contributors: ThemeHigh
 Donate link: https://themehigh.com/
 Tags: Accordion FAQ, WordPress FAQ Plugin, FAQ Plugin, WordPress FAQ, FAQ Widget 
 Requires at least: 5.0
-Tested up to: 7.0
-Stable tag: 1.5.5
+Tested up to: 7.1
+Stable tag: 1.5.6
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -144,6 +144,9 @@ You can add as many FAQs you need. There is no limit defined.
 11.Sample FAQ template.
 
 == Changelog ==
+
+= 1.5.6 =
+* Added WordPress 7.1 compatibility.
 
 = 1.5.5 =
 * Added WordPress 7.0 compatibility.
